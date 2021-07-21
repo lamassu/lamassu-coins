3e61ea025d5bbfa38009e1ea0b5cb2c19be70edd
José Oliveira <josepfo.w@gmail.com>
2021-07-21 23:49:26 +0100

    feat: add multiple display units

--- config/consts.js
+++ config/consts.js
@@ -19,10 +19,18 @@ const CRYPTO_CURRENCIES = [
     daemon: 'bitcoind',
     defaultPort: 8332,
     unitScale: 8,
-    displayScale: 5,
     zeroConf: true,
     type: 'coin',
-    displayCode: 'mBTC'
+    units:{
+      full: {
+        displayScale: 8,
+        displayCode: 'BTC'
+      },
+      mili: {
+        displayScale: 5,
+        displayCode: 'mBTC'
+      }
+    }
   },
   {
     cryptoCode: COINS.BCH,
@@ -32,10 +40,18 @@ const CRYPTO_CURRENCIES = [
     daemon: 'bitcoincashd',
     defaultPort: 8335,
     unitScale: 8,
-    displayScale: 5,
     zeroConf: true,
     type: 'coin',
-    displayCode: 'mBCH'
+    units:{
+      full: {
+        displayScale: 8,
+        displayCode: 'BCH'
+      },
+      mili: {
+        displayScale: 5,
+        displayCode: 'mBCH'
+      }
+    }
   },
   {
     cryptoCode: COINS.DASH,
@@ -45,10 +61,18 @@ const CRYPTO_CURRENCIES = [
     daemon: 'dashd',
     defaultPort: 9998,
     unitScale: 8,
-    displayScale: 5,
     zeroConf: true,
     type: 'coin',
-    displayCode: 'mDASH'
+    units:{
+      full: {
+        displayScale: 8,
+        displayCode: 'DASH'
+      },
+      mili: {
+        displayScale: 5,
+        displayCode: 'mDASH'
+      }
+    }
   },
   {
     cryptoCode: COINS.ETH,
@@ -58,10 +82,18 @@ const CRYPTO_CURRENCIES = [
     daemon: 'geth',
     defaultPort: 8545,
     unitScale: 18,
-    displayScale: 15,
-    zeroConf: true,
+    zeroConf: false,
     type: 'coin',
-    displayCode: 'mETH'
+    units:{
+      full: {
+        displayScale: 18,
+        displayCode: 'ETH'
+      },
+      mili: {
+        displayScale: 15,
+        displayCode: 'mETH'
+      }
+    }
   },
   {
     cryptoCode: COINS.LTC,
@@ -71,19 +103,31 @@ const CRYPTO_CURRENCIES = [
     daemon: 'litecoind',
     defaultPort: 9332,
     unitScale: 8,
-    displayScale: 5,
     zeroConf: true,
     type: 'coin',
-    displayCode: 'mLTC'
+    units:{
+      full: {
+        displayScale: 8,
+        displayCode: 'LTC'
+      },
+      mili: {
+        displayScale: 5,
+        displayCode: 'mLTC'
+      }
+    }
   },
   {
     cryptoCode: COINS.USDT,
     display: 'Tether',
     unitScale: 6,
-    displayScale: 0,
     contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
     type: 'erc-20',
-    displayCode: 'USDT'
+    units:{
+      full: {
+        displayScale: 6,
+        displayCode: 'USDT'
+      }
+    }
   },
   {
     cryptoCode: COINS.ZEC,
@@ -93,10 +137,18 @@ const CRYPTO_CURRENCIES = [
     daemon: 'zcashd',
     defaultPort: 8232,
     unitScale: 8,
-    displayScale: 5,
     zeroConf: true,
     type: 'coin',
-    displayCode: 'mZEC'
+    units:{
+      full: {
+        displayScale: 8,
+        displayCode: 'ZEC'
+      },
+      mili: {
+        displayScale: 5,
+        displayCode: 'mZEC'
+      }
+    }
   }
 ]
 
