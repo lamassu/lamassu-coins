f07afab5a90d0a2e2ce1c5df2afbba123da49c86
José Oliveira <josepfo.w@gmail.com>
2022-03-09 23:48:49 +0000

    fix: typo on display name

--- config/consts.js
+++ config/consts.js
@@ -222,7 +222,7 @@ const CRYPTOS = [
   },
   {
     cryptoCode: COINS.LN,
-    display: 'Lighting Network',
+    display: 'Lightning Network',
     code: 'ln',
     configFile: null,
     daemon: null,
