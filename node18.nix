f9c8d0bac02c3e884ae9003ee3265a18a9ccafd5
siiky <github-siiky@net-c.cat>
2024-03-06 15:31:04 +0000

    feat: add Nix shell file for Node.js 18

---
+++ node18.nix
@@ -0,0 +1,16 @@
+with import (fetchTarball {
+  name = "nixpkgs-23.05";
+  url = https://github.com/NixOS/nixpkgs/archive/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8.tar.gz;
+  sha256 = "17v6wigks04x1d63a2wcd7cc4z9ca6qr0f4xvw1pdw83f8a3c0nj";
+}) {};
+
+stdenv.mkDerivation {
+    name = "node";
+    buildInputs = [
+        nodejs-18_x
+        openssl
+    ];
+    shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+    '';
+}
