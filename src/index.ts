0a0fb566646d22722d9e544e435c9e172f915899
siiky <github-siiky@net-c.cat>
2024-03-11 17:35:29 +0000

    refactor: use TS export notation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { COINS, ALL_CRYPTOS, CRYPTO_CURRENCIES } from './config/consts'
 export const addressDetector = require('./plugins/detector')
-export const utils = require('./utils')
+export * as utils from './utils'
