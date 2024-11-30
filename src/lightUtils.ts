17c961c419f0d216dbd7ce3e637c1f9d25b79507
Rafael <rafael.taranto@protonmail.com>
2024-11-30 08:32:30 +0000

    chore: use lightUtils on react

---
+++ src/lightUtils.ts
@@ -0,0 +1,36 @@
+import _ from 'lodash/fp'
+
+import {CRYPTO_CURRENCIES} from './config/consts'
+
+export function cryptoCurrencies() {
+  return CRYPTO_CURRENCIES
+}
+
+export function getEquivalentCode(cryptoCurrency: string) {
+  switch (cryptoCurrency) {
+    case 'USDT_TRON':
+      return 'USDT';
+    case 'LN':
+      return 'BTC';
+    default:
+      return cryptoCurrency;
+  }
+}
+
+export function toUnit (cryptoAtoms: any, cryptoCode: string) {
+  const cryptoRec = getCryptoCurrency(cryptoCode)
+  const unitScale = cryptoRec.unitScale
+  return cryptoAtoms.shiftedBy(-unitScale)
+}
+
+/* TODO: make cryptoCode more restrictive: https://www.typescriptlang.org/docs/handbook/enums.html */
+export function getCryptoCurrency (cryptoCode: string) {
+  const cryptoCurrency = _.find(['cryptoCode', cryptoCode], cryptoCurrencies())
+  if (!cryptoCurrency) throw new Error(`Unsupported crypto: ${cryptoCode}`)
+  return cryptoCurrency
+}
+
+export function formatCryptoAddress(cryptoCode: string = '', address: string = '') {
+  return cryptoCode === 'BCH' ? address.replace('bitcoincash:', '') : address
+}
+
